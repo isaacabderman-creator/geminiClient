6d03e93bcac408e607f65ca74a9c3b82a05b00de
isaacabderman-creator <hei.isaac.2@gmail.com>
2026-09-10 06:53:10 +0300

    feat: add validation to response

--- src/client.ts
+++ src/client.ts
@@ -1,16 +1,31 @@
 import { GoogleGenAI } from "@google/genai";
 import { InteractionSchema, type Interaction } from "./schema.js";
+import type { ClientError } from "./error.js";
 
 const ai = new GoogleGenAI({});
 export const getInteraction = async (
   model: string,
   prompt: string = "Hello",
-): Promise<Interaction> => {
-  const response = await ai.interactions.create({
-    model: model,
-    input: prompt,
-  });
-  const interaction: Interaction = InteractionSchema.parse(response);
+): Promise<Interaction | ClientError> => {
+  let response;
+  try {
+    response = await ai.interactions.create({
+      model: model,
+      input: prompt,
+    });
+  } catch (e) {
+    return { type: "network_error", error: e };
+  }
+  if (response.status === "failed") {
+    return {
+      type: "http_error",
+      status: 500,
+      statusText: JSON.stringify(response.errors ?? "Interaction failed"),
+    };
+  }
+  const parsedResult = InteractionSchema.safeParse(response);
+  if (!parsedResult.success)
+    return { type: "schema_error", issues: [parsedResult.error] };
 
-  return interaction;
+  return parsedResult.data;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import * as fs from "fs";
 import { RESPONSE, createResponse } from "./scripts/createResponseSample.js";
 import { getInteraction } from "./client.js";
+import readlineSync from "readline-sync";
 if (!fs.existsSync(RESPONSE)) createResponse();
 
-console.log(await getInteraction("gemini-3.5-flash-lite", "How ya doing?"));
+const userInput = readlineSync.question("User: ").trim();
+const response = await getInteraction("gemini-3.5-flash-lite", userInput);
+
+if ("type" in response) {
+  switch (response.type) {
+    case "http_error":
+      console.log("HTTP Error: ", response.status, response.statusText);
+      break;
+    case "schema_error":
+      console.log("Schema Error: ", response.issues);
+      break;
+    case "network_error":
+      console.log("Network Error: ", response.error);
+      break;
+  }
+} else {
+  console.log("AI: ", response.output_text);
+}
